dea433fe717ce2614b99d018f8395afb55a05033
Сергей Яговкин <scorpionupgrade@mail.ru>
2020-04-10 09:59:43 +0400

    feat: add caps and shift simple mode

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
 
 class Button {
-  constructor(code, enSymbol, ruSymbol, buttonSize = 'x1', color = '1') {
+  constructor(code, enSymbol, ruSymbol, enAltSymbol = 'upper', ruAltSymbol = 'upper', buttonSize = 'x1', color = '1') {
     this.code = code;
     this.symbols = [enSymbol, ruSymbol];
+    this.altSymbols = [enAltSymbol, ruAltSymbol];
     this.setButtonSize(buttonSize);
     this.setButtonColor(color);
   }
@@ -55,8 +56,17 @@ class Button {
     }
   }
 
-  getSymbol(keyboardLayoutIndex) {
-    return this.symbols[keyboardLayoutIndex];
+  getSymbol(keyboardLayoutIndex, leterUpperMode, numberUpperMode) {
+    const upperMode = (!leterUpperMode && numberUpperMode) || (leterUpperMode && !numberUpperMode);
+    if (this.altSymbols[keyboardLayoutIndex] === 'upper') {
+      return upperMode
+        ? this.symbols[keyboardLayoutIndex].toUpperCase()
+        : this.symbols[keyboardLayoutIndex];
+    }
+
+    return numberUpperMode
+      ? this.altSymbols[keyboardLayoutIndex]
+      : this.symbols[keyboardLayoutIndex];
   }
 }
 
@@ -79,26 +89,28 @@ let keyboardLayoutIndex = parseInt(localStorage.keyboardLayoutIndex, 10);
 if (Number.isNaN(keyboardLayoutIndex)) keyboardLayoutIndex = 0;
 const keydowns = [];
 
+let capsMode = false;
+let shiftMode = false;
+
 const inputedSymbols = [];
 
 const buttons = [
-  new Button('Backquote', '`', '`'),
-  new Button('Digit1', '1', '1'),
-  new Button('Digit2', '2', '2'),
-  new Button('Digit3', '3', '3'),
-  new Button('Digit4', '4', '4'),
-  new Button('Digit5', '5', '5'),
-  new Button('Digit6', '6', '6'),
-  new Button('Digit7', '7', '7'),
-  new Button('Digit8', '8', '8'),
-  new Button('Digit9', '9', '9'),
-  new Button('Digit0', '0', '0'),
-  new Button('Minus', '-', '-'),
-  new Button('Equal', '=', '='),
-  new Button('Backspace', 'Backspace', 'Backspace', 'x2', '2'),
+  new Button('Backquote', '`', 'ё', '~'),
+  new Button('Digit1', '1', '1', '!', '!'),
+  new Button('Digit2', '2', '2', '@', '@'),
+  new Button('Digit3', '3', '3', '#', '#'),
+  new Button('Digit4', '4', '4', '$', '$'),
+  new Button('Digit5', '5', '5', '%', '%'),
+  new Button('Digit6', '6', '6', '^', '^'),
+  new Button('Digit7', '7', '7', '&', '&'),
+  new Button('Digit8', '8', '8', '*', '*'),
+  new Button('Digit9', '9', '9', '(', '('),
+  new Button('Digit0', '0', '0', ')', ')'),
+  new Button('Minus', '-', '-', '_', '_'),
+  new Button('Equal', '=', '=', '+', '+'),
+  new Button('Backspace', 'Backspace', 'Backspace', 'Backspace', 'Backspace', 'x2', '2'),
 
-
-  new Button('Tab', 'Tab', 'Tab', 'x1.5', '2'),
+  new Button('Tab', 'Tab', 'Tab', 'Tab', 'Tab', 'x1.5', '2'),
   new Button('KeyQ', 'q', 'й'),
   new Button('KeyW', 'w', 'ц'),
   new Button('KeyE', 'e', 'у'),
@@ -109,11 +121,11 @@ const buttons = [
   new Button('KeyI', 'i', 'ш'),
   new Button('KeyO', 'o', 'щ'),
   new Button('KeyP', 'p', 'з'),
-  new Button('BracketLeft', '[', 'х'),
-  new Button('BracketRight', ']', 'ъ'),
-  new Button('Backslash', '\\', '\\', 'x1.5'),
+  new Button('BracketLeft', '[', 'х', '{'),
+  new Button('BracketRight', ']', 'ъ', '}'),
+  new Button('Backslash', '\\', '\\', '|', '|', 'x1.5'),
 
-  new Button('CapsLock', 'Caps Lock', 'Caps Lock', 'x1.75', '2'),
+  new Button('CapsLock', 'Caps Lock', 'Caps Lock', 'Caps Lock', 'Caps Lock', 'x1.75', '2'),
   new Button('KeyA', 'a', 'ф'),
   new Button('KeyS', 's', 'ы'),
   new Button('KeyD', 'd', 'в'),
@@ -123,11 +135,11 @@ const buttons = [
   new Button('KeyJ', 'j', 'о'),
   new Button('KeyK', 'k', 'л'),
   new Button('KeyL', 'l', 'д'),
-  new Button('Semicolon', ';', 'ж'),
-  new Button('Quote', '\'', 'э'),
-  new Button('Enter', 'Enter', 'Enter', 'x2.25', '2'),
+  new Button('Semicolon', ';', 'ж', ':'),
+  new Button('Quote', '\'', 'э', '"'),
+  new Button('Enter', 'Enter', 'Enter', 'Enter', 'Enter', 'x2.25', '2'),
 
-  new Button('ShiftLeft', 'Shift', 'Shift', 'x2.25', '2'),
+  new Button('ShiftLeft', 'Shift', 'Shift', 'Shift', 'Shift', 'x2.25', '2'),
   new Button('KeyZ', 'z', 'я'),
   new Button('KeyX', 'x', 'ч'),
   new Button('KeyC', 'c', 'с'),
@@ -135,22 +147,22 @@ const buttons = [
   new Button('KeyB', 'b', 'и'),
   new Button('KeyN', 'n', 'т'),
   new Button('KeyM', 'm', 'ь'),
-  new Button('Comma', ',', 'б'),
-  new Button('Period', '.', 'ю'),
-  new Button('Slash', '/', '.'),
-  new Button('ShiftRight', 'Shift', 'Shift', 'x1.25', '2'),
-  new Button('ArrowUp', 'ᐃ', 'ᐃ', 'x0.75', '3'),
-  new Button('ContextMenu', 'CM', 'CM', 'x0.75', '3'),
+  new Button('Comma', ',', 'б', '<'),
+  new Button('Period', '.', 'ю', '>'),
+  new Button('Slash', '/', '.', '?', ','),
+  new Button('ShiftRight', 'Shift', 'Shift', 'Shift', 'Shift', 'x1.25', '2'),
+  new Button('ArrowUp', 'ᐃ', 'ᐃ', 'ᐃ', 'ᐃ', 'x0.75', '3'),
+  new Button('ContextMenu', 'CM', 'CM', 'CM', 'CM', 'x0.75', '3'),
 
-  new Button('ControlLeft', 'Ctrl', 'Ctrl', 'x1.25', '2'),
-  new Button('MetaLeft', 'Win', 'Win', 'x1.25', '2'),
-  new Button('AltLeft', 'Alt', 'Alt', 'x1.25', '2'),
-  new Button('Space', ' ', ' ', 'x6.25'),
-  new Button('AltRight', 'Alt Gr', 'Alt Gr', 'x1.25', '2'),
-  new Button('MetaRight', 'Win', 'Win', 'x1.5', '2'),
-  new Button('ArrowLeft', 'ᐊ', 'ᐊ', 'x0.75', '3'),
-  new Button('ArrowDown', 'ᐁ', 'ᐁ', 'x0.75', '3'),
-  new Button('ArrowRight', 'ᐅ', 'ᐅ', 'x0.75', '3'),
+  new Button('ControlLeft', 'Ctrl', 'Ctrl', 'Ctrl', 'Ctrl', 'x1.25', '2'),
+  new Button('MetaLeft', 'Win', 'Win', 'Win', 'Win', 'x1.25', '2'),
+  new Button('AltLeft', 'Alt', 'Alt', 'Alt', 'Alt', 'x1.25', '2'),
+  new Button('Space', ' ', ' ', ' ', ' ', 'x6.25'),
+  new Button('AltRight', 'Alt Gr', 'Alt Gr', 'Alt Gr', 'Alt Gr', 'x1.25', '2'),
+  new Button('MetaRight', 'Win', 'Win', 'Win', 'Win', 'x1.5', '2'),
+  new Button('ArrowLeft', 'ᐊ', 'ᐊ', 'ᐊ', 'ᐊ', 'x0.75', '3'),
+  new Button('ArrowDown', 'ᐁ', 'ᐁ', 'ᐁ', 'ᐁ', 'x0.75', '3'),
+  new Button('ArrowRight', 'ᐅ', 'ᐅ', 'ᐅ', 'ᐅ', 'x0.75', '3'),
 ];
 
 
@@ -160,6 +172,23 @@ function setActiveButton(button) {
 
 function unsetActiveButton(button) {
   button.classList.remove('keyboard__button_active');
+}
+
+function onAnimationEnd() {
+  if (this.dataset.keydown === 'false') { unsetActiveButton(this); }
+}
+
+function fillKeyboardLayout() {
+  for (let i = 0; i < buttons.length; i += 1) {
+    const button = keyboard.children[i];
+    button.textContent = buttons[i].getSymbol(keyboardLayoutIndex, capsMode, shiftMode);
+  }
+}
+
+function changeKeyboardLayout() {
+  keyboardLayoutIndex = (keyboardLayoutIndex + 1) % 2;
+  localStorage.setItem('keyboardLayoutIndex', keyboardLayoutIndex);
+  fillKeyboardLayout();
 }
 
 function addSymbolToTextArea(symbol) {
@@ -174,7 +203,13 @@ function addSymbolToTextArea(symbol) {
       inputedSymbols.push('\t');
       break;
     case 'Caps Lock':
+      capsMode = !capsMode;
+      fillKeyboardLayout();
+      break;
     case 'Shift':
+      shiftMode = !shiftMode;
+      fillKeyboardLayout();
+      break;
     case 'Ctrl':
     case 'Win':
     case 'Alt':
@@ -186,22 +221,6 @@ function addSymbolToTextArea(symbol) {
       break;
   }
   input.textContent = inputedSymbols.join('');
-}
-
-function onAnimationEnd() {
-  if (this.dataset.keydown === 'false') { unsetActiveButton(this); }
-}
-
-function fillKeyboardLayout() {
-  for (let i = 0; i < buttons.length; i += 1) {
-    keyboard.children[i].textContent = buttons[i].getSymbol(keyboardLayoutIndex);
-  }
-}
-
-function changeKeyboardLayout() {
-  keyboardLayoutIndex = (keyboardLayoutIndex + 1) % 2;
-  localStorage.setItem('keyboardLayoutIndex', keyboardLayoutIndex);
-  fillKeyboardLayout();
 }
 
 function setKeydown(button, state) {
@@ -226,7 +245,7 @@ function fillKeyboard() {
     keyboardButton.classList.add('keyboard__button');
     keyboardButton.dataset.code = buttons[i].code;
     keyboardButton.dataset.keydown = 'false';
-    keyboardButton.textContent = buttons[i].getSymbol(keyboardLayoutIndex);
+    keyboardButton.textContent = buttons[i].getSymbol(keyboardLayoutIndex, capsMode, shiftMode);
     keyboardButton.addEventListener('click', onClickButton);
     keyboardButton.addEventListener('transitionend', onAnimationEnd);
     if (buttons[i].buttonSize !== undefined) keyboardButton.classList.add(buttons[i].buttonSize);
